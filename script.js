17d068205c4fbee3dffba9f03c60a08ec2a16183
Maianne Thornton <maianne.thornton@gmail.com>
2022-08-08 14:44:32 -0400

    feat: JS functionality complete

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const loveMe = document.querySelector('.loveMe')
-const times = document.querySelector('.times')
+const times = document.querySelector('#times')
+
+let timesClicked = 0
+
 
 let clickTime = 0
 
 // Creating a double click addEventListener type
 loveMe.addEventListener('click', (e) => {
-    if(clickTime === 0) {
+    if (clickTime === 0) {
         clickTime = new Date().getTime()
     } else {
         // if new date time is less than 800ms
-        if((new Date().getTime() - clickTime) < 800) {
+        if ((new Date().getTime() - clickTime) < 800) {
             // console.log(123);
             createHeart(e)
             clickTime = 0
@@ -19,3 +22,34 @@ loveMe.addEventListener('click', (e) => {
     }
 })
 
+const createHeart = (e) => {
+    const heart = document.createElement('i')
+    heart.classList.add('fas')
+    heart.classList.add('fa-heart')
+
+    const x = e.clientX
+    const y = e.clientY
+
+    // console.log('x:',x, 'y:',y);
+
+    const leftOffset = e.target.offsetLeft
+    const topOffset = e.target.offsetTop
+
+    // console.log('leftOffset:',leftOffset, 'topOffset:',topOffset);
+
+    // x & y values *inside* the image
+    const xInside = x - leftOffset
+    const yInside = y - topOffset
+
+    // console.log('xInside:', xInside, 'yInside:',yInside);
+
+    heart.style.top = `${yInside}px`
+    heart.style.left = `${xInside}px`
+
+    loveMe.appendChild(heart)
+
+    times.innerText = ++timesClicked
+
+    // Removes extra heart elements from the DOM after 1 sec
+    setTimeout(() => heart.remove(), 1000);
+}
